2745a5044a072ad9c44c7647237a42b7f5c8b752
accraze <accraze@gmail.com>
2014-03-30 19:08:16 -0700

    debugging wall

--- Assets/Scripts/FindSphereTrigger.cs
+++ Assets/Scripts/FindSphereTrigger.cs
@@ -6,14 +6,18 @@ public class FindSphereTrigger : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		string[] tags = { "Handhold", "Foothold", "Multihold", "Checkpoint", "Goal"};
-
+		Color[] colors = {Color.blue, Color.cyan, Color.white, Color.green, Color.red};
+		int i = 0;
 		foreach(string tag in tags)
 		{
 			GameObject[] spheres = GameObject.FindGameObjectsWithTag (tag);
 
 			foreach (GameObject s in spheres) {
 					s.GetComponent<Collider> ().isTrigger = true;
+				s.GetComponent<Renderer>().material.color = colors[i];
+					
 			}
+			i++;
 		}
 	}
 	
