5744d3b7440acab3f047b7a486f3a6ad763cf941
accraze <accraze@gmail.com>
2014-03-30 19:41:09 -0700

    adding joe's script and prefabs lolz

---
+++ Assets/Scripts/TriggerBoxScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerBoxScript : MonoBehaviour {
+	bool boxSizeDebugOn;
+	bool entranceDebugOn;
+	// Use this for initialization
+	void Start () {
+		boxSizeDebugOn = false;
+		entranceDebugOn = false;
+		renderer.enabled = false;
+		GameObject chi = transform.GetChild(0).gameObject;
+		chi.renderer.enabled= false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//renderer.enabled=entranceDebugOn;
+		if (Input.GetKeyDown ("u")){
+			boxSizeDebugOn = !boxSizeDebugOn;
+		}
+		if (Input.GetKeyDown ("i")){
+			entranceDebugOn = !entranceDebugOn;
+		}
+		renderer.enabled = boxSizeDebugOn;
+	}
+	void OnTriggerEnter(Collider other) {
+			GameObject chi = transform.GetChild(0).gameObject;
+			chi.renderer.enabled= entranceDebugOn && true;
+    }
+	void OnTriggerExit(Collider other) {
+			GameObject chi = transform.GetChild(0).gameObject;
+			chi.renderer.enabled= false;
+    }
+}
