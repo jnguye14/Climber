a696bb3678fcd2224c2bc6ad0fa4aaaafcbe1a3f
Jordan Nguyen <Jordan.N.Nguyen@asu.edu>
2014-04-21 17:14:32 -0700

    updates: Music, Credits script, and bar manager

---
+++ Assets/Scripts/BalanceSlider.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalanceSlider : MonoBehaviour
+{
+	public Rect area = new Rect(10f,10f,50f,10f);
+	public Texture2D bgTexture;
+	public Texture2D fillTexture;
+	private float buffer;
+
+    private float barFill = 0.5f;
+    public float BarFill
+    {
+        get
+        {
+            return barFill;
+        }
+        set
+        {
+            barFill = Mathf.Clamp(value, 0.0f, 1.0f);
+        }
+    }
+
+    public bool Full
+    {
+        get
+        {
+            return barFill == 1.0f;
+        }
+    }
+
+    public bool Empty
+    {
+        get
+        {
+            return barFill == 0.0f;
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+	{
+        if (bgTexture == null)
+        {
+            bgTexture = new Texture2D(1,1);
+            bgTexture.SetPixel(0, 0, Color.white);
+            bgTexture.Apply();
+        }
+        if (fillTexture == null)
+        {
+            int resolution = 256; // power of two > 0
+            fillTexture = new Texture2D(resolution, resolution);
+            for (int i = 0; i < fillTexture.width; i++)
+            {
+                for (int j = 0; j < fillTexture.height; j++)
+                {
+                    int someVal = (i - resolution / 2) * (i - resolution / 2) + (j - resolution / 2) * (j - resolution / 2);
+                    if (someVal < resolution * resolution / 4)
+                    {
+                        float numerator = Mathf.Sqrt(someVal);
+                        float denominator = (float)resolution / 2.0f;
+                        fillTexture.SetPixel(i, j, Color.Lerp(Color.red, Color.yellow, numerator / denominator));
+                    }
+                    else
+                    {
+                        fillTexture.SetPixel(i, j, Color.clear);
+                    }
+                }
+            }
+            fillTexture.Apply();
+        }
+
+        buffer = area.height*0.75f;
+	}
+
+	void OnGUI()
+	{
+		GUI.depth = -1; // draw on top of everything
+
+		// draw the background:
+        GUI.DrawTexture(adjRect(area), bgTexture);
+
+		// draw the filled-in part
+        Rect temp = adjRect(new Rect(area.x + area.width * barFill, area.y - buffer * 0.25f, 0,0));
+        temp.x -= buffer * Screen.height / 100.0f;
+        temp.width = buffer * 2 * Screen.height/100.0f;
+        temp.height = buffer * 2 * Screen.height/100.0f;
+		GUI.DrawTexture  (temp, fillTexture);
+	}
+
+    // returns Rectangle adjusted to screen size
+    Rect adjRect(Rect r)
+    {
+        return new Rect(
+                r.x * Screen.width / 100.0f,
+                r.y * Screen.height / 100.0f,
+                r.width * Screen.width / 100.0f,
+                r.height * Screen.height / 100.0f);
+    }
+
+    public void increaseBar(float amount)
+    {
+        BarFill += amount;
+    }
+
+    public void decreaseBar(float amount)
+    {
+        BarFill -= amount;
+    }
+
+    public void setAmount(float amount)
+    {
+        BarFill = amount;
+    }
+}
--- Assets/Scripts/Bar.cs
+++ Assets/Scripts/Bar.cs
@@ -1,91 +1,128 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System; // for EventArgs
+using UnityEngine;
+using System.Collections; // not needed
 
 public class Bar : MonoBehaviour
 {
-	public Rect area = new Rect(20f,40f,100f,50f);
-	public Texture2D bgTexture;
-	public Texture2D fillTexture;
-	public float buffer = 5f;
-	
-	public bool full = false;
-	public float barFill = 0.5f;
-	public bool empty = false;
+    public Rect area = new Rect(0f, 0f, 10f, 5f);
+    public Texture2D bgTexture;
+    public Texture2D fillTexture;
+    public float buffer = 5f;
 
-	// Use this for initialization
-	void Start ()
-	{
-		barFill = 0.5f; // start game half way
+    public delegate void EmptyEventHandler(object sender, EventArgs e);
+    public static event EmptyEventHandler EmptyEvent;
+    protected virtual void OnEmptyEvent(EventArgs e)
+    {
+        if (EmptyEvent != null)
+        {
+            EmptyEvent(this, e);
+        }
+    }
+
+    private float barFill = 0.5f;
+    public float BarFill
+    {
+        get
+        {
+            return barFill;
+        }
+        set
+        {
+            barFill = Mathf.Clamp(value, 0.0f, 1.0f);
+            if (Empty)
+            {
+                OnEmptyEvent(EventArgs.Empty);
+            }
+        }
+    }
+
+    public bool Full
+    {
+        get
+        {
+            return barFill == 1.0f;
+        }
+    }
+
+    public bool Empty
+    {
+        get
+        {
+            return barFill == 0.0f;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
         if (bgTexture == null)
         {
-            bgTexture = new Texture2D(1,1);
-            bgTexture.SetPixel(0, 0, Color.white);
-            bgTexture.Apply();
+            Texture2D temp = new Texture2D(1, 1);
+            temp.SetPixel(0, 0, Color.white);
+            temp.Apply();
+            bgTexture = temp;
         }
         if (fillTexture == null)
         {
-            fillTexture = new Texture2D(1, 1);
-            fillTexture.SetPixel(0, 0, Color.white);
-            fillTexture.Apply();
+            Texture2D temp = new Texture2D(1, 1);
+            temp.SetPixel(0, 0, Color.white);
+            temp.Apply();
+            fillTexture = temp;
         }
-	}
-	
-	// Update is called once per frame
-	void Update () { }
+    }
 
-	void OnGUI()
-	{
-		GUI.depth = -1; // draw on top of everything
+    void OnGUI()
+    {
+        GUI.depth = -1; // draw on top of everything
 
-		//GUIStyle style;
+        //GUIStyle style;
 
-		// draw the background:
-		GUI.BeginGroup (area);
-		GUI.DrawTexture(new Rect (0f,0f, area.width, area.height), bgTexture);
-		
-		// change color of fill based on current amount
-		if(barFill < 0.33f)
-		{
-			GUI.color = Color.red;
-		}
-		else if(barFill < 0.66f)
-		{
-			GUI.color = Color.yellow;
-		}
-		else
-		{
-			GUI.color = Color.green;
-		}
+        Rect area = adjRect(this.area);
+        // draw the background:
+        GUI.BeginGroup(area);
+        GUI.DrawTexture(new Rect(0f, 0f, area.width, area.height), bgTexture);
 
-		// draw the filled-in part
-		GUI.DrawTexture  (new Rect (buffer,buffer, (area.width-2*buffer)*barFill, area.height - 2*buffer), fillTexture);
-		GUI.EndGroup ();
-	} 
+        // change color of fill based on current amount
+        if (barFill < 0.33f)
+        {
+            GUI.color = Color.red;
+        }
+        else if (barFill < 0.66f)
+        {
+            GUI.color = Color.yellow;
+        }
+        else
+        {
+            GUI.color = Color.green;
+        }
 
-	public void increaseBar(float amount)
-	{
-		empty = false;
-		barFill += amount;
-		if(barFill >= 1f)
-		{
-			barFill = 1f;
-			full = true; // or send bar filled event
-		}
-	}
-	
-	public void decreaseBar(float amount)
-	{
-		full = false;
-		barFill -= amount;
-		if(barFill <= 0f)
-		{
-			barFill = 0f;
-			empty = true; // or send bar empty event
-		}
-	}
-	
-	public void setAmount(float amount)
-	{
-		barFill = Mathf.Clamp(amount, 0f, 1f);
-	}
+        // draw the filled-in part
+        GUI.DrawTexture(new Rect(buffer, buffer, (area.width - 2 * buffer) * barFill, area.height - 2 * buffer), fillTexture);
+        GUI.EndGroup();
+    }
+
+    // returns Rectangle adjusted to screen size
+    Rect adjRect(Rect r)
+    {
+        return new Rect(
+                r.x * Screen.width / 100.0f,
+                r.y * Screen.height / 100.0f,
+                r.width * Screen.width / 100.0f,
+                r.height * Screen.height / 100.0f);
+    }
+
+    public void increaseBar(float amount)
+    {
+        BarFill += amount;
+    }
+
+    public void decreaseBar(float amount)
+    {
+        BarFill -= amount;
+    }
+
+    public void setAmount(float amount)
+    {
+        BarFill = amount;
+    }
 }
