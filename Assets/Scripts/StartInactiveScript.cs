85ab478dd4797afde255f81c03c7e0449dd58e5b
joefs <joe.f.spiro@gmail.com>
2014-04-25 02:05:56 -0700

    Halfway Commit
    "Modified the first person controller (increased jump height).
    
    Put in some invisible walls to make the cliff more manageable at the
    higher speed.
    
    Currently working on the transition from FPC to climber camera for the
    three different walls."

---
+++ Assets/Scripts/StartInactiveScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartInactiveScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		/*
+		MonoBehaviour[] comps = this.gameObject.GetComponents<MonoBehaviour>();
+		 
+		foreach(MonoBehaviour c in comps)
+		{
+		  c.enabled = false;
+		}
+		*/
+		this.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
